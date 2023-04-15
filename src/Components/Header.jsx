e3d62749beb9a2e8afdca6f9d38468194ca19372
Emon <emon.mhk69@gmail.com>
2023-04-15 23:11:12 +0600

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is headerr</h2>
+        <div className="navbar bg-sky-200">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Authentication PRO</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
