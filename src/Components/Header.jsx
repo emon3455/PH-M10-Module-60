b411f0d95336831de9b1a01c0375ef3056ed11e9
Emon <emon.mhk69@gmail.com>
2023-04-16 02:18:00 +0600

    user information stored and signout button done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Header = () => {
+
+    const {user,logOut} = useContext(AuthContext);
+
+    const handleSignOut=() =>{
+        logOut()
+        .then(()=>{})
+        .cath(er=>{
+            console.log(er.message);
+        })
+    }   
+
     return (
         <div className="navbar bg-sky-200">
             <div className="flex-1">
@@ -9,9 +21,21 @@ const Header = () => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li>
+                        {
+                            user 
+                            ? 
+                            <p>
+                            <span>{user.email}</span>
+                            <button onClick={handleSignOut} className='btn btn-xs'>Log Out</button>
+                            </p> 
+                            : 
+                            <Link to="/login">Log In</Link>
+                        }
+                    </li>
                 </ul>
             </div>
         </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { app } from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
@@ -18,10 +18,28 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth,email,password);
     }
 
+    const logOut=()=>{
+        return signOut(auth);
+    }
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+        })
+
+        return()=>{
+            unsubscribe();
+        }
+
+    },[])
+
+
     const authInfo ={
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
